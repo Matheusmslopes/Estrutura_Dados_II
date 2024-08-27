cf65a45554c5c2a07905a77d3e56546e635f9268
Matheus <matheusmuruci@outlook.com>
2024-08-27 17:26:31 -0300

    feat[BinaryTree]: Adicionado alguns comentários e algumas pequenas mudanças: Retirado printf de dentro da função Deletar, Foram adicionadas 2 funções para mostrar a árvore ao usuário.

--- 3 - Arvores/BinaryTree.c
+++ 3 - Arvores/BinaryTree.c
@@ -1,19 +1,22 @@
-                                                /*Nome: Matheus Muruci de Souza Lopes*/
+/*Nome: Matheus Muruci de Souza Lopes*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
+// Estrutura criada para
 struct Node {
   int info;
   struct Node *direita;
   struct Node *esquerda;
 };
 
+// Função para criar um nó
 struct Node *CriarNode(int valor) {
   struct Node *NovoNode = (struct Node *)malloc(sizeof(struct Node));
 
   if (NovoNode == NULL) {
     printf("Erro ao criar novo nó \n");
-    exit(1);
+    return NULL;
   }
 
   NovoNode->info = valor;
@@ -23,6 +26,7 @@ struct Node *CriarNode(int valor) {
   return NovoNode;
 }
 
+// Função para encontrar o nó com o menor valor da subarvore(Nesse caso ta pegando o valor mais a esquerda da subarvore a direita)
 struct Node *encontrarMinimo(struct Node *raiz) {
   struct Node *minimo = raiz;
   while (minimo->esquerda != NULL) {
@@ -31,6 +35,7 @@ struct Node *encontrarMinimo(struct Node *raiz) {
   return minimo;
 }
 
+// Função para inserir um valor na árvore
 struct Node *Inserir(struct Node *raiz, int valor) {
   if (raiz == NULL) {
     raiz = CriarNode(valor);
@@ -44,6 +49,7 @@ struct Node *Inserir(struct Node *raiz, int valor) {
   return raiz;
 }
 
+// Função para pesquisar um nó na árvore
 struct Node *Pesquisar(struct Node *raiz, int valor) {
   struct Node *Pesquisado = raiz;
   if (raiz == NULL) {
@@ -64,9 +70,9 @@ struct Node *Pesquisar(struct Node *raiz, int valor) {
   return Pesquisado;
 }
 
+// Função para deletar um nó da árvore
 struct Node *Deletar(struct Node *raiz, int valor) {
   if (raiz == NULL) {
-    printf("Arvore não existe \n");
     return NULL;
   }
 
@@ -78,22 +84,43 @@ struct Node *Deletar(struct Node *raiz, int valor) {
     if (raiz->direita == NULL) {
       struct Node *tmp = raiz->esquerda;
       free(raiz);
-      printf("o nó com valor %i foi deletado \n", valor);
       return tmp;
     } else if (raiz->esquerda == NULL) {
       struct Node *tmp = raiz->direita;
       free(raiz);
-      printf("o nó com valor %i foi deletado \n", valor);
       return tmp;
     } else {
       struct Node *tmp = encontrarMinimo(raiz->direita);
       raiz->info = tmp->info;
       raiz->direita = Deletar(raiz->direita, tmp->info);
-      printf("o nó com valor %i foi deletado \n", valor);
     }
-    printf("Não foi encontrado o nó com valor %i para ser deletado \n", valor);
   }
   return raiz;
+}
+
+/*
+Ambas as funções void imprimeNo(int c, int b)  e void mostraArvore(struct
+Node *a, int b) foram pegas diretamente do diretório original do professor Gustavo
+(https://github.com/gnrochabr/Estrutura_Dados_II/blob/main/3%20-%20Arvores/BinaryTree.c),
+servindo apenas com o intuito de mostrar a árvore antes e depois do processo de exclusão. Na função void mostraArvore(struct Node *a, int b) Foram realizadas apenas mudanças para adequar o nome de uma variável e da struct.
+*/
+
+// Função auxiliar para imprimir um caractere precedido por uma quantidade específica de espaços
+void imprimeNo(int c, int b) {
+  int i;
+  for (i = 0; i < b; i++)
+    printf("   ");
+  printf("%i\n", c);
+}
+
+// Função para exibir a árvore no formato esquerda-raiz-direita segundo Sedgewick
+void mostraArvore(struct Node *a, int b) {
+  if (a == NULL) {
+    return;
+  }
+  mostraArvore(a->direita, b + 1);
+  imprimeNo(a->info, b); // Convertendo para caractere para imprimir
+  mostraArvore(a->esquerda, b + 1);
 }
 
 int main(void) {
@@ -118,7 +145,15 @@ int main(void) {
   printf("Qual nó você deseja deletar: ");
   scanf("%i", &deletar);
 
+  // Serve para mostrar a árvore antes do processo de exclusão
+  printf("\nAntes: \n");
+  mostraArvore(raiz, 3);
+  
   Deletar(raiz, deletar);
+
+  // Serve para mostrar a árvore depois do processo de exclusão
+  printf("\nDepois: \n");
+  mostraArvore(raiz, 3);
 
   return 0;
 }
